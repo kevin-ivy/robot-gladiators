33c1d9082dc42ddb80b26123c38409dae64ca471
kIvy1189 <kevindivy@gmail.com>
2020-03-30 17:27:59 -0500

    establish enemy array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,22 @@
+//Game States
+//"WIN" - Player robot has defeated all enemy robots
+//  *Fight all enemy robots
+//  *Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
 // Initialize player robot name and starting stats
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// Log the above values
-console.log(playerName, playerHealth, playerAttack);
-
-//Initialize enemy robot and stats
-var enemyName = "Gato";
+//Initialize enemy robots and stats
+var enemyNames = ["Pacifier", "Overlord", "Gamma"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 //Create fight function
-var fight = function() {
+var fight = function(enemyName) {
 //Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -82,4 +85,6 @@ var fight = function() {
 
 };
 
-fight ();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
