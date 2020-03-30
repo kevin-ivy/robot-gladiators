0a2d15b6bd6fcb9e7200db19749d86d1a6c065a0
kIvy1189 <kevindivy@gmail.com>
2020-03-30 16:23:25 -0500

    Set robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// Name Robot
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("Our robot's name is " + playerName);
+
+// This creates a function named "Fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
